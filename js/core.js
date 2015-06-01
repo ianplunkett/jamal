5ab26bece682938a8ac725c58015cc8081b93e92
Ian Plunkett <ian.plunkett@gmail.com>
2015-05-31 21:15:16 -0700

    Moving functionality into core and out of eval. Not sure how this refactoring will play out. Eval is pretty messy right now.  I'd like to adopt a model where each of the functions in core handle all the operations on the ast.  Right now, lots of that occurs in Eval.

--- js/core.js
+++ js/core.js
@@ -4,63 +4,155 @@ let Exception = require('./exception.js');
 
 function addition() {
     return {
-        type: 'arithmetic',
-        fn: (a,b) => a+b,
-        string: '+'
+        type : 'arithmetic',
+        string : '+',
+        fn : (a,b) => a+b
     };
 }
 
 function subtraction() {
     return {
-        type: 'arithmetic',
-        fn: (a,b) => a-b,
-        string: '-'
+        type : 'arithmetic',
+        string : '-',
+        fn : (a,b) => a-b
     };
 }
 
 function multiplication() {
     return {
-        type: 'arithmetic',
-        fn: (a,b) => a*b,
-        string: '*'
+        type : 'arithmetic',
+        string : '*',
+        fn : (a,b) => a*b
     };
 }
 
 function division() {
     return {
-        type: 'arithmetic',
-        fn: (a,b) => parseInt(a/b),
-        string: '/'
+        type : 'arithmetic',
+        string : '/',
+        fn : (a,b) => parseInt(a/b)
+    };
+}
+
+function def_() {
+    return  {
+        type : 'special',
+        string : 'def!'
+    };
+}
+
+function let_() {
+    return {
+        type : 'special',
+        string : 'let*'
+    };
+}
+
+function do_() {
+    return {
+        type : 'special',
+        string : 'do'
+    };
+}
+
+function if_() {
+    return {
+        type : 'special',
+        string : 'if'
+    };
+}
+
+function fn_() {
+    return {
+        type : 'special',
+        string : 'fn*'
+    };
+}
+
+function list() {
+    return {
+        type : 'special',
+        string : 'fn*'
+    };
+}
+
+function is_list() {
+    return {
+        type : 'special',
+        string : 'list'
+    };
+}
+
+function count() {
+    return {
+        type: 'special',
+        string : 'count'
+    };
+}
+
+function is_empty() {
+    return {
+        type : 'special',
+        string : 'empty?'
+    };
+}
+
+function is_equal() {
+    return {
+        type : 'equality',
+        string : '='
+    };
+}
+
+function is_greater_than() {
+    return {
+        type : 'equality',
+        string : '>'
+    };
+}
+
+function is_greater_than_or_equal() {
+    return {
+        type : 'equality',
+        string : '>='
+    };
+}
+
+function is_less_than() {
+    return {
+        type : 'equality',
+        string : '<'
+    };
+}
+
+function is_less_than_or_equal() {
+    return {
+        type : 'equality',
+        string : '<='
     };
 }
 
 function Core(env) {
 
     let ns = {
-        '+' : addition,
-        '-' : subtraction,
-        '*' : multiplication,
-        '/' : division,
-        'def!' : {
-            type: 'special',
-            string: 'def!'
-        },
-        'let*' : {
-            type: 'special',
-            string: 'let*'
-        },
-        'do' : {
-            type: 'special',
-            string: 'do'
-        },
-        'if':  {
-            type: 'special',
-            string: 'if'
-        },
-        'fn*' : {
-            type: 'special',
-            string: 'fn*'
-        }
+        '+' : addition(),
+        '-' : subtraction(),
+        '*' : multiplication(),
+        '/' : division(),
+        'def!' : def_(),
+        'let*' : let_(),
+        'do' : do_(),
+        'if' :  if_(),
+        'fn*' : fn_(),
+        'list': list(),
+        'list?': is_list(),
+        'count' : count(),
+        'empty?' : is_empty(),
+        '=' : is_equal(),
+        '>' : is_greater_than(),
+        '>=' : is_greater_than_or_equal(),
+        '<' : is_less_than(),
+        '<=' : is_less_than_or_equal()
     };
 
     for (let specialForm in ns) {
