141fdee21c2bb1ec42cbaef150db61fb6c4d0a92
Ian Plunkett <ian.plunkett@gmail.com>
2015-06-13 16:08:30 -0700

    formatting changes

--- js/tokenizer.js
+++ js/tokenizer.js
@@ -1,9 +1,10 @@
 'use strict';
 // The following regular expression (PCRE) will match all mal tokens.
 function Tokenizer(input) {
-    let regexp = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/;
-    let tokens = input.split(regexp);
-    let filteredTokens = [];
+    let regexp = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/,
+        tokens = input.split(regexp),
+        filteredTokens = [];
+    
     for(let i = 0; i < tokens.length; i++) {
         if(tokens[i] !== '') {
             filteredTokens.push(tokens[i]);
