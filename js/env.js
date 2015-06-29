8b45be863e92d5495d9a58e4f58b3246d25d0004
Ian Plunkett <ian.plunkett@gmail.com>
2015-06-28 19:33:41 -0700

    adding strict env module

--- js/env.js
+++ js/env.js
@@ -1,5 +1,8 @@
+'use strict';
+let Exception = require('./exception.js');
+
 function Env(outer, binds, exprs) {
-    self = this;
+    let self = this;
     if (typeof binds === 'object' && binds.length > 0) {
         binds.map(
             function(currentValue, index, array) {
@@ -23,7 +26,7 @@ Env.prototype.find = function(key) {
     } else if (typeof this.outer === 'object') {
         return this.outer.find(key);
     } else {
-        return false;
+        throw Exception("symbol not found");
     }
 };
 
