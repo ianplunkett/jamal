212d14559f5d2d8d77828cf37fe5d1f6f9311499
Ian Plunkett <ian.plunkett@gmail.com>
2015-06-29 23:57:17 -0700

    passing step1, step2, step3 with the exception of the with-meta test from step1. not sure i understand the use case still

--- js/printer.js
+++ js/printer.js
@@ -7,29 +7,36 @@ function Printer(malData) {
     return this;
 }
 
-Printer.prototype.build_str = function(ast) {
+Printer.prototype.build_str = function(ast, is_child) {
 
     let output = '';
 
-    // TOOD: we can't assume this will work. we are creating an array
-    // of the object keys and blindly considering it as the only key.
     switch (ast.form) {
         case 'list':
+            //TODO - clean up the code to determine if we add a trailing space
             output = output.concat(ast.begin);
             for (let child of ast.value) {
                 if (child.form === 'list' || child.form === 'pair') {
-                    output = output.concat(this.build_str(child));
+                    if (ast.value.length - 1 === ast.value.indexOf(child)) {
+                        output = output.concat(this.build_str(child, false));
+                    } else {
+                        output = output.concat(this.build_str(child, true));
+                    }
                 } else if (ast.value.length - 1 === ast.value.indexOf(child)) {
                     output = output.concat(child.value);
                 } else {
                     output = output.concat(child.value)+" ";
                 }
             }
-            return output.concat(ast.end);
+            if (is_child) {
+                return output.concat(ast.end+" ");
+            } else {
+                return output.concat(ast.end);
+            }
         case 'pair':
             output = output.concat(ast.begin+ " ");
             if (ast.value.form === 'list' || ast.value.form === 'pair') {
-                output = output.concat(this.build_str(ast.value));
+                output = output.concat(this.build_str(ast.value, false));
             } else {
                 output = output.concat(ast.value.value);
             }
@@ -42,7 +49,7 @@ Printer.prototype.build_str = function(ast) {
 };
 
 Printer.prototype.pr_str = function() {
-    return this.build_str(this.malData);
+    return this.build_str(this.malData, false);
 };
 
 
