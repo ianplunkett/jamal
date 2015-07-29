b36475def4ff0e662731bf24034a354070c42272
Ian Plunkett <ian.plunkett@gmail.com>
2015-07-29 08:07:14 -0700

    Building out code generated methods

--- generated/JAMAL/eval_ast.js
+++ generated/JAMAL/eval_ast.js
@@ -5,13 +5,49 @@
 
 var Env = require('Env');
 
-function eval_ast(){
+/**
+* @return eval_ast 
+*/
+function eval_ast(ast, env){
     //Constructor
-
-    this.AST = null;
-    this.ENV = null;
-
+    this.AST = ast;
+    this.ENV = env;
+    return this;
 }
+
+
+/**
+* @return AST {[object Object]} 
+*/
+eval_ast.prototype.list = function(){
+    return this.AST.firstChild();
+};
+
+
+/**
+* @return AST {[object Object]} 
+*/
+eval_ast.prototype.symbol = function(){
+    return this.ENV.get(this.AST);
+};
+
+
+/**
+* @return AST {[object Object]} 
+*/
+eval_ast.prototype.vector = function(){
+    //TODO: Implement Me 
+    return this.AST;
+};
+
+
+/**
+* @return AST {[object Object]} 
+*/
+eval_ast.prototype.hash_map = function(){
+    //TODO: Implement Me 
+    return this.AST;
+};
 
 
 
