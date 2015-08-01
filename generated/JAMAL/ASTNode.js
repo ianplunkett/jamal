e44fcbce5c4372f64395c0db5ebb8ec57d740896
Ian Plunkett <ian.plunkett@gmail.com>
2015-08-01 12:10:48 -0700

    Building out a tree structure.
    Doubly-linked lists.  Parent/Child and Sibling relationships.  Only able
    to create new nodes and add children at the moment.

--- generated/JAMAL/ASTNode.js
+++ generated/JAMAL/ASTNode.js
@@ -1,43 +1,57 @@
 'use strict';
 
-function ASTNode(type, value){
-    this.type = type;
-    this.value = value;
+function ASTNode(data) {
+    this.data        = data,
+    this.next        = null,
+    this.previous    = null,
+    this.parent      = null,
+    this.first_child = null,
+    this.last_child  = null;
 }
 
-
-/**
-* @return ASTNode {[object Object]} 
-*/
-ASTNode.prototype.parent = function(){
-    
-    return false;
+ASTNode.prototype.addFirstChild = function(ast_node) {
+    this.first_child = ast_node;
+    this.last_child = ast_node;
+    ast_node.parent = this;
 };
 
-
-/**
-* @return ASTNode {[object Object]} 
-*/
-ASTNode.prototype.children = function(){
-    return false;
+ASTNode.prototype.addNextChild = function(ast_node) {
+    ast_node.previous = this.last_child;
+    this.last_child.next = ast_node;
+    this.last_child = ast_node;
+    ast_node.parent = this;
 };
 
-
+const proto = ASTNode.prototype;
 /**
-* @return ASTNode {[object Object]} 
+Object.defineProperty(proto, "value", {
+    get: ()       => this.value,
+    set: (value)  => this.value = value
+});
+
+Object.defineProperty(proto, "parent", {
+    get: ()       => this.parent,
+    set: (parent) => this.parent = parent
+});
+
+Object.defineProperty(proto, "previous", {
+    get: ()     => this.previous,
+    set: (previous) => this.previous = previous
+});
+
+Object.defineProperty(proto, "next", {
+    get: ()      => this.next,
+    set: (next) => this.next = next
+});
+
+Object.defineProperty(proto, "first_child", {
+    get: ()     => this.first_child,
+    set: (first_child) => this.first_child = first_child
+});
+
+Object.defineProperty(proto, "last_child", {
+    get: ()      => this.last_child,
+    set: (last_child) => this.last_child = last_child
+});
 */
-ASTNode.prototype.left_sibling = function(){
-    return false;
-};
-
-
-/**
-* @return ASTNode {[object Object]} 
-*/
-ASTNode.prototype.right_sibling = function(){
-    return false;
-};
-
-
-
 module.exports = ASTNode;
